fae1c779397de8d04853632958078e0b0f8b9a42
Harrison Brock <harrison@harrisonbrock.com>
2019-02-04 17:22:32 -0500

    create stub test in main

--- src/com/harrisonbrock/Main.java
+++ src/com/harrisonbrock/Main.java
@@ -3,6 +3,15 @@ package com.harrisonbrock;
 public class Main {
 
     public static void main(String[] args) {
+
         System.out.println("Hello World!");
+
+        Customer jane = new Customer("Jane", 45.25);
+        Customer bob = new Customer("Bob", 33.14);
+
+        VendingMachine food = new VendingMachine("Food");
+        VendingMachine drink = new VendingMachine("Drink");
+        VendingMachine office = new VendingMachine("Office");
+
     }
 }
