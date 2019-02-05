e6ca6ae122c4143be8fdcfdb49b1402f9e5caab8
Harrison Brock <harrison@harrisonbrock.com>
2019-02-04 19:00:14 -0500

    refactor main

--- src/com/harrisonbrock/Main.java
+++ src/com/harrisonbrock/Main.java
@@ -20,6 +20,27 @@ public class Main {
         Snack soda = new Snack("Soda", 24, 2.50, drink.getId());
         Snack water = new Snack("Water", 20, 2.75, drink.getId());
 
+        jane.buyItems(3, soda);
+        System.out.println("Customer 1 buys 3 of snack 4.  Cash on hand: " + jane.getCashOnHand() + " quantity of snack 4: " + soda.getQuantity());
+        jane.buyItems(1, pretzel);
+        System.out.println("Customer 1 buys 1 of snack 3.  Cash on hand: " + jane.getCashOnHand() +  " quantity of snack 3: " + pretzel.getQuantity());
+
+        bob.buyItems(2, water);
+        System.out.println("Customer 2 buys 3 of snack 4.  Cash on hand: " + bob.getCashOnHand() +  " quantity of snack 3: " + water.getQuantity());
+
+        jane.addCash(10.00);
+        System.out.println("Customer 1 Cash on hand: " + jane.getCashOnHand());
+
+        jane.buyItems(1, chocalateBar);
+        System.out.println("Customer 1 buys 1 of snack 2.  Cash on hand: " + jane.getCashOnHand() +  " quantity of snack 3 " + chocalateBar.getQuantity());
+
+        pretzel.addQuantity(12);
+        System.out.println("quantity of snack 3: " + pretzel.getQuantity());
+
+        bob.buyItems(3, pretzel);
+        System.out.println("Customer 2 buys 3 of snack 3.  Cash on hand: " + bob.getCashOnHand() +  " quantity of snack 3: " + pretzel.getQuantity());
+
+
 
 
     }
